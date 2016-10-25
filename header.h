9795639e6552af640f4c9a89e54f06dbc86b4f76
Ignacs <seellk@gmail.com>
2016-10-25 20:45:51 +0800

    Move

--- header.h
+++ header.h
@@ -1,8 +1,12 @@
 
 #include <errno.h>
+
+#define BUFF_LEN 4096
+
 #define FILE_TRADING_DAY    "TradingDay" 
 #define FILE_HOLIDAY        "Holiday"
 #define TSE_DATE_CLASSIFIC  "TSE.db3"
+
 #define DEBUG_OUTPUT( formats, args...) do{ \
                 fprintf(stderr, "%s:%d > ", __FUNCTION__, __LINE__); \
                 fprintf(stderr, formats, ##args); \
@@ -82,3 +86,63 @@ void output_err(unsigned int err)
             break;
     }
 }
+
+unsigned int wcsConvToU_10b(wchar_t *wcs)
+{
+    wchar_t *ptr = NULL ;
+    int i = 0;
+    char buf[BUFF_LEN];
+
+    if( NULL == wcs)
+        return 0;
+    ptr = wcs;
+    
+    memset(buf, 0x0, sizeof(buf));
+DEBUG_OUTPUT(">>> string [%ls] \n", wcs);
+    for(i = 0; 1; i++)
+    {
+DEBUG_OUTPUT(">>> %lc \n",  *(ptr+i));
+        switch(*(ptr+i))
+        {
+            case L'0':
+                buf[i] = '0';
+                break;
+            case L'1':
+                buf[i] = '1';
+                break;
+            case L'2':
+                buf[i] = '2';
+                break;
+            case L'3':
+                buf[i] = '3';
+                break;
+            case L'4':
+                buf[i] = '4';
+                break;
+            case L'5':
+                buf[i] = '5';
+                break;
+            case L'6':
+                buf[i] = '6';
+                break;
+            case L'7':
+                buf[i] = '7';
+                break;
+            case L'8':
+                buf[i] = '8';
+                break;
+            case L'9':
+                buf[i] = '9';
+                break;
+            default : 
+DEBUG_OUTPUT(">>> %ld \n",  strtol( buf, NULL, 10));
+                return strtol( buf, NULL, 10);
+                break;
+        }
+    }
+
+DEBUG_OUTPUT(">>> %ld \n",  strtol( buf, NULL, 10));
+    return 0;
+}
+
+
