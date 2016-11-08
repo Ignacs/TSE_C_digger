918b72fd73efa259239b972d6cd8013abe905c67
Ignacs <seellk@gmail.com>
2016-11-08 21:02:23 +0800

    1. Calculate VR from ordered data by date

--- libVR.c
+++ libVR.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "header.h"
-
+#define TODAY_PRIV      2 // the lastday privriledge weigth
 
 //  line data sequence: date      stockNum        tradeNum        volume      open        high        low     close       sign        diff        buy     buyVol      sell        sellVol     epr
 //
@@ -25,6 +25,7 @@ double calculate_VR(struct stock_daily *pDataCal, unsigned int day)
     int i = 0;
     unsigned long volSumPosDay = 0;
     unsigned long volSumNegDay = 0;
+    double ret =0.0;
 
     if(NULL == pDataCal)
     {
@@ -43,28 +44,25 @@ double calculate_VR(struct stock_daily *pDataCal, unsigned int day)
         // 在這裡是要將股數換成張數所以除以1000
         if(SIGN_UP == (pDataCal+i)->sign)
         {
-            DEBUG_OUTPUT("%d day is pos\n", i);
-            volSumPosDay += (unsigned long)((pDataCal+i)->stockNum/1000);
+            volSumPosDay += (pDataCal+i)->stockNum;
+            DEBUG_OUTPUT("%d day is + = %lu, total = %lu\n", i, (pDataCal+i)->stockNum, volSumPosDay);
         }
         else if(SIGN_DOWN == (pDataCal+i)->sign)
         {
-            DEBUG_OUTPUT("%d day is neg\n", i);
-            volSumNegDay += (unsigned long)((pDataCal+i)->stockNum/1000);
+            volSumNegDay += (pDataCal+i)->stockNum;
+            DEBUG_OUTPUT("%d day is - = %lu, total = %lu\n", i, (pDataCal+i)->stockNum, volSumNegDay);
         }
     }
     DEBUG_OUTPUT("postive day %lu.\n", volSumPosDay);
     DEBUG_OUTPUT("negative day %lu .\n", volSumNegDay);
 
+    ret = 0.0;
     if(0 < volSumNegDay)
     {
-        DEBUG_OUTPUT("result %f .\n", (double )((volSumPosDay*1.0) / (volSumNegDay*1.0)));
-        return (volSumPosDay / volSumNegDay);
+        ret = (double )((volSumPosDay*1.0) / (volSumNegDay*1.0));
+        DEBUG_OUTPUT("result %f .\n", ret);
     }
-    else
-    {
-        DEBUG_OUTPUT("negative day is ZERO .\n");
-        return 0.0;
-    }
+    return ret;
 }
 
 //
@@ -79,6 +77,8 @@ int indicator(struct stock_daily **pStockData, int length, int dayVR, int dayVRM
     unsigned int i  = 0, j = 0;
     int ret = 0;
     struct stock_daily *pData=*pStockData;
+    double aVR[LEN_DAYS_ORD] = {0.0};
+    double dVRMA1 = 0.0, dVRMA2 = 0.0;
 
     if(NULL == pStockData)
     {
@@ -96,7 +96,7 @@ int indicator(struct stock_daily **pStockData, int length, int dayVR, int dayVRM
 
     DEBUG_OUTPUT( "dayVR = %d\n", dayVR);
     // if VR is too short to calculate VRMA.
-    if(dayVRMA >= dayVR)
+    if(dayVRMA >= dayVR && LEN_DAYS_ORD < dayVR)
     {
         DEBUG_OUTPUT( "ERR: Too short than VRMA\n");
         return 0;
@@ -104,8 +104,8 @@ int indicator(struct stock_daily **pStockData, int length, int dayVR, int dayVRM
 
     DEBUG_OUTPUT( "length = %d\n", length);
     // if history data is too short to calculate.
-    // for precesion , the result must compare (dayVR+dayVRMA) days.
-    if( (dayVR+dayVRMA) >= length )
+    // for precesion , the result must compare (dayVR+dayVRMA+1) days.
+    if( (dayVR+dayVRMA+1) >= length )
     {
         DEBUG_OUTPUT( "ERR: history too short to calculate!!\n");
         return 0;
@@ -119,10 +119,35 @@ int indicator(struct stock_daily **pStockData, int length, int dayVR, int dayVRM
     {
         DEBUG_OUTPUT( "File open successfully\n");
 
-        for(i = 0 ; i< dayVRMA; i++)
+        // VR need n+1 days to calculate
+        for(i = 0; i <= (dayVR); i++)
         {
-            calculate_VR((*pStockData)+i, dayVR);
+            aVR[i] = calculate_VR((*pStockData)+i, dayVR);
+            DEBUG_OUTPUT( "date %lu (%d) days ago's VR= %f\n", ((*pStockData)+i)->date, i, aVR[i]);
         }
+        /*
+
+        //  Indicator must get (dayVRMA+1) for accuracy
+        // calculate VRMA from today to (dayVRMA-1)
+        // First, calculate the (dayVRMA) ago
+        DEBUG_OUTPUT( "dVRMA1 (%d) days ago = %f\n", dayVRMA, dVRMA1);
+        for(i = (dayVRMA-1); i >= 0; i--)
+        {
+            //  calculate VRMA
+            dVRMA1 = ((dVRMA1*(dayVRMA-1)) + (calculate_VR((*pStockData)+i, dayVR)*TODAY_PRIV)) / (dayVRMA+1);
+            DEBUG_OUTPUT( "dVRMA1 (%d) days ago =%f\n", i, dVRMA1);
+        }
+
+        // calculate
+        DEBUG_OUTPUT( "dVRMA1 (%d) days ago = %f\n", dayVRMA, dVRMA2);
+        for(i = (dayVRMA-1); i >= 0; i--)
+        {
+            //  calculate VRMA
+            dVRMA1 = ((dVRMA1*(dayVRMA-1)) + (calculate_VR((*pStockData)+i, dayVR)*TODAY_PRIV)) / (dayVRMA+1);
+            DEBUG_OUTPUT( "dVRMA1 (%d) days ago =%f\n", i, dVRMA1);
+        }
+        */
+
 
         fclose(pOoutFp);
     }
@@ -130,7 +155,6 @@ int indicator(struct stock_daily **pStockData, int length, int dayVR, int dayVRM
     {
         output_err(FILE_OPEN_FAIL);
     }
-
 
     return 0;
 }
